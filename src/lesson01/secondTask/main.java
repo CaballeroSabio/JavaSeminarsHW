43bb43cb26e5198004955b564ac4dcebd7e1716d
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-12-13 19:51:06 +0300

    created the new code

--- src/lesson01/secondTask/main.java
+++ src/lesson01/secondTask/main.java
@@ -1,2 +1,15 @@
-package lesson01.secondTask;public class main {
+//2.Вывести все простые числа от 1 до 100
+
+package lesson01.secondTask;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        int i = 100;
+        do {
+            System.out.println("Element: " + i);
+            i -= 1;
+        } while (i <= 1);
+    }
 }
