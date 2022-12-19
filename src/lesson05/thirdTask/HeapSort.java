8405f1e2b40665d5c2bdb67afe1cc4f05a1b55f3
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-12-20 00:21:23 +0300

    created the new code

--- src/lesson05/thirdTask/HeapSort.java
+++ src/lesson05/thirdTask/HeapSort.java
@@ -1,2 +1,52 @@
-package lesson05.thirdTask;public class HeapSort {
+//3.* Реализовать алгоритм пирамидальной сортировки (HeapSort).
+
+package lesson05.thirdTask;
+
+import java.util.Arrays;
+
+public class HeapSort {
+    public static void main(String[] args) {
+        int[] initArray = new int[]{17, 32, 1, 4, 25, 17, 0, 3, 10, 7, 64, 1};
+        System.out.println("Initial array:");
+        System.out.println(Arrays.toString(initArray));
+
+        heapSort(initArray, initArray.length);
+
+        System.out.println("Sorted array:");
+        System.out.println(Arrays.toString(initArray));
+    }
+
+    public static void buildTree(int[] tree, int sortLength) {
+        // Для всех вершин дерева начиная с середины сортируемой части массива
+        // (движемся влево до нулевого индекса)
+        for (int i = sortLength / 2 - 1; i >= 0; i--) {
+            int maxIndex;
+            // Если у вершины два листа, выбираем больший
+            if (i * 2 + 2 <= sortLength - 1) {
+                if (tree[i * 2 + 2] > tree[i * 2 + 1]) maxIndex = i * 2 + 2;
+                else maxIndex = i * 2 + 1;
+                // Иначе один лист
+            } else maxIndex = i * 2 + 1;
+            // Сравниваем лист с максимальным значением с вершиной, при большем значении
+            // дочернего элемента меняем его местами с вершиной
+            if (tree[i] < tree[maxIndex]) {
+                int temp = tree[i];
+                tree[i] = tree[maxIndex];
+                tree[maxIndex] = temp;
+            }
+        }
+    }
+
+    public static void heapSort(int[] sortArray, int sortLength) {
+        // Выход из рекурсии: длина сортируемой части равна 0
+        if (sortLength == 0) return;
+        // Выстраивание элементов части массива в виде сортирующего дерева
+        buildTree(sortArray, sortLength);
+        // Замена местами первого и последнего элемента в сортируемой части
+        int temp = sortArray[0];
+        sortArray[0] = sortArray[sortLength - 1];
+        sortArray[sortLength - 1] = temp;
+        // Рекурсивно построение дерева и замена элементов для n - 1 элементов массива
+        heapSort(sortArray, sortLength - 1);
+    }
 }
